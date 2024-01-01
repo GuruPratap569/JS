5b79ae6797506fc0e24b5eeb42043e5285cb1c50
GuruPratap569 <155042477+GuruPratap569@users.noreply.github.com>
2024-01-01 17:08:05 +0000

    js

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true
+let num =Number(score)
+// console.log(num);
+// console.log(typeof (num));
+// console.log(score);
+// console.log(typeof score);
+
+let str =""
+let boolean = Boolean(str)
+console.log(boolean);
+
+
+let  str1 = "Guru"
+let str2 = " Maya"
+
+let str3 = str1+str2
+// console.log(str3);   
+
+
+console.log(1+"2");   
+console.log(typeof ("1"+2));   
+console.log(1+"2"+3);   
+console.log(1+2+3);   
+console.log(typeof(1+2+3)+"4");   
